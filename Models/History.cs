1155bef30f57d509a8838dfea3109fbc9f1b6c62
rzezniczekk <kamil.rzezniczek@explitia.com>
2024-12-14 11:05:45 +0100

    Added history class

---
+++ Models/History.cs
@@ -0,0 +1,10 @@
+﻿namespace MonikaSAP.Models
+{
+    public class History
+    {
+        public string Number { get; set; }
+        public string Formula { get; set; }
+        public double Result { get; set; }
+        public string Reason { get; set; }
+    }
+}
